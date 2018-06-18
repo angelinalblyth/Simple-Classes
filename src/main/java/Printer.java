0cfac2103bddfed8a580ee069c0b4b03bfe4eb7b
Angelina Blyth <angelinalblyth@hotmail.com>
2018-06-18 16:34:50 +0100

    Added printer extensions

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -3,34 +3,48 @@ public class Printer {
     private int paper;
     private int pages;
     private int copies;
+    private int toner;
 
-    public Printer(int paper, int pages, int copies){
+
+    public Printer(int paper, int pages, int copies, int toner){
         this.paper = paper;
         this.pages = pages;
         this.copies = copies;
+        this.toner = toner;
     }
 
     public int getSheets(){
-        return this.paper;
+        return paper;
     }
 
     public int printed() {
-        return this.pages * this.copies;
-    }
-
-    public int printNew(){
-      return this.paper -= printed();
+        return pages * copies;
     }
 
 
+    public int subtractToner() {
+        return toner -= pages;
+    }
 
-    public boolean enoughPaper(){
-        if (this.paper >= printed()){
-            return true;
+
+//    public boolean enoughPaper(){
+//        if (paper >= printed()){
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+
+    public int print(){
+        if (paper >= printed()){
+            return paper -= printed();
         } else {
-            return false;
+            return paper;
         }
+
     }
 
-
+    public int refill() {
+        return paper = 500;
+    }
 }
--- src/test/java/TestPrinter.java
+++ src/test/java/TestPrinter.java
@@ -6,26 +6,53 @@ public class TestPrinter {
 
     @Test
     public void getSheets(){
-       Printer printer = new Printer(500, 0, 0);
+       Printer printer = new Printer(500, 0, 0, 0);
        assertEquals(500, printer.getSheets());
     }
 
     @Test
     public void printed(){
-        Printer printer = new Printer ( 500, 2, 10);
+        Printer printer = new Printer ( 500, 2, 10, 10);
         assertEquals(20, printer.printed());
     }
 
     @Test
     public void printNew(){
-        Printer printer = new Printer ( 500, 2, 10);
+        Printer printer = new Printer ( 500, 2, 10, 10);
         assertEquals(480, printer.printNew());
     }
 
     @Test
     public void notEnoughPaper(){
-        Printer printer = new Printer ( 18, 2, 10);
+        Printer printer = new Printer ( 18, 2, 10, 10);
         assertEquals(false, printer.enoughPaper());
     }
+
+    @Test
+    public void notAbleToPrint(){
+        Printer printer = new Printer ( 18, 2, 10, 10);
+        assertEquals(18, printer.print());
+    }
+
+    @Test
+    public void ableToPrint(){
+        Printer printer = new Printer ( 500, 2, 10, 10);
+        assertEquals(480, printer.print());
+    }
+
+    @Test
+    public void refillPrinter(){
+        Printer printer = new Printer ( 18, 2, 10, 10);
+        assertEquals(500, printer.refill());
+    }
+
+    @Test
+    public void subtractToner(){
+        Printer printer = new Printer ( 500, 2, 10, 10);
+        assertEquals(8, printer.subtractToner());
+    }
+
+
+
 
 }
